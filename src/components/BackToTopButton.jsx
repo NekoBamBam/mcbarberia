5744557ac8859542b6a8f36d1742cc98d30672dc
Lawcito <121973056+Lawcito@users.noreply.github.com>
2025-11-17 01:27:01 -0300

    back to top button adition

---
+++ src/components/BackToTopButton.jsx
@@ -0,0 +1,52 @@
+import { useEffect, useState } from "react";
+import React from "react";
+
+const BackToTopButton = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const toggleVisibility = () => {
+    if (window.pageYOffset > 300) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", toggleVisibility);
+
+    return () => {
+      window.removeEventListener("scroll", toggleVisibility);
+    };
+  }, []);
+
+  return (
+    <button
+      onClick={scrollToTop}
+      className={`back-to-top ${isVisible ? "visible" : ""}`}
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        fill="0000"
+        className="bi bi-arrow-up-short"
+        viewBox="0 0 16 16"
+      >
+        <path
+          fillRule="evenodd"
+          d="M8 12a.5.5 0 0 0 .5-.5V5.707l2.146 2.147a.5.5 0 0 0 .708-.708l-3-3a.5.5 0 0 0-.708 0l-3 3a.5.5 0 1 0 .708.708L7.5 5.707V11.5a.5.5 0 0 0 .5.5"
+        />
+      </svg>
+    </button>
+  );
+};
+
+export default BackToTopButton;
